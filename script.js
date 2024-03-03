35c2f996d3718ab71fa786c1a46a47eed8dd43ab
LcasFerrei <lucasferreiraceara@gmail.com>
2024-03-02 21:29:03 -0300

    Adicionei o login animado e fiz pequenas mudanças no script

--- script.js
+++ script.js
@@ -65,3 +65,17 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+
+
+// login script
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+
+registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
+});
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
+});
